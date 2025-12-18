a9a615415eb2e6d130f5fd24c588043a4709be3c
Jericho Delos Reyes <jericho.dlsreyes@gmail.com>
2025-12-18 09:41:16 +0800

    REDO

--- src/pages/dashboard/index.js
+++
@@ -1,4 +0,0 @@
-export { default as StudentDashboard } from './StudentDashboard';
-export { default as FacultyDashboard } from './FacultyDashboard';
-export { default as GuardDashboard } from './GuardDashboard';
-export { default as AdminDashboard } from './AdminDashboard';
--- src/pages/directory/index.js
+++
@@ -1,2 +0,0 @@
-export { default as EmergencyDirectory } from './EmergencyDirectory';
-export { default as BuildingDirectory } from './BuildingDirectory';
